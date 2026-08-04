8d8fa7c2c2cb1c1037fd00632fd2dcd4562c3596
Alina Peng <alinapeng@Alinas-MacBook-Air.local>
2026-08-04 14:18:01 -0400

    Add React Router and navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
 import Favorites from "./pages/Favorites";
 
 export default function App() {
-  return <Favorites />;
+  return (
+    <BrowserRouter>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/favorites" element={<Favorites />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <h2>Nova</h2>
+
+      <Link to="/">Home</Link>{" "}
+      <Link to="/favorites">Favorites</Link>
+    </nav>
+  );
+}
