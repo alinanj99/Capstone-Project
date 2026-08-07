d60c80344a166d6891f6e29092c52c93df7447ba
Alina Peng <alinapeng@Alinas-MacBook-Air.local>
2026-08-07 11:30:40 -0400

    Update AI Insights

--- frontend/src/pages/AIInsights.jsx
+++ frontend/src/pages/AIInsights.jsx
@@ -4,7 +4,7 @@ export default function AIInsights() {
       <h1>AI Insights</h1>
 
       <p className="intro">
-  Nova uses Gemini AI to explain NASA's Astronomy Picture of the Day in simple English.
+  Nova uses Gemini AI to generate simple explanations for NASA's Astronomy Picture of the Day.
 </p>
 
 <h2>How to Use</h2>
